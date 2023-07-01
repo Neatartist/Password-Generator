b9d20046b7cc044e4ad1e0e311c6167bf6516871
Daniel Van Kirk <davk0810@gmail.com>
2023-06-30 23:23:42 -0400

    upload02

--- Assets/css/script.js
+++ Assets/css/script.js
@@ -1,8 +1,8 @@
 // character arrays
-var numarray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-var arrayspecial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/']
-var Upperletter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z']
-var lowerletter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x', 'y', 'z']
+var numarray = ['1234567890']
+var arrayspecial = ['!@#$%^&*()_+']
+var Upperletter = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+var lowerletter = ['abcdefghijklmnopqrstuvwxyz']
 // assignment code
 var generateBtn = document.querySelector("#generate");
 // write password to the #password input
@@ -11,23 +11,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
+// generate password function
 function generatePassword() {
   var possiblepassword = '';
   var actualpassword = '';
   // requirements for password length
-  var passwordlength = parseInt(prompt("A password is usually required to be between 8 and 14 characters. Enter how many characters you want yours to be."),12);
+  var passwordlength = parseInt(prompt("A password is usually required to be between 8 and 12 characters. Enter how many characters you want yours to be.",8));
   //
   if (isNaN (passwordlength)) {   
-    alert("Please enter a number between 8 and 14 .");
+    alert("Please enter a number between 8 and 12.");
     return "Please enter a valid number.";
   }
   if (passwordlength < 8) {
-    alert("Please enter a number between 8 and 14 .");
+    alert("Please enter a number between 8 and 12.");
     return "Please enter a valid number.";
   }
   if (passwordlength > 12) {
-    alert("Please enter a number between 8 and 14 .");
+    alert("Please enter a number between 8 and 12.");
     return "Please enter a valid number.";
   }
   // confirmations for lower case letter  requirements
